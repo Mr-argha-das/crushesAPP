bb322cc69e42281af39056069bc77effe6b19a49
das-globally-web <das.globally.web@gmail.com>
2025-03-21 15:50:39 +0530

    changes

---
+++ lib/home/controller/service/home.service.dart
@@ -0,0 +1,13 @@
+import 'package:crush_dating/home/model/user.natch.model.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart' hide Headers;
+part 'home.service.g.dart';
+
+@RestApi(baseUrl: "https://crushes-backend-fzuu4.ondigitalocean.app")
+abstract class HomeService {
+  factory HomeService(Dio dio, {String baseUrl}) = _HomeService;
+
+  @POST("/user/match-users/")
+  Future<UserMatchResModel> userMatches();
+}
